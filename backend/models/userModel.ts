807cfc63d6c210e893990b7c409a468c65190bcf
Paulo Carvalho <pauloej.carvalho@gmail.com>
2022-06-29 14:36:07 -0300

    feat(mongodb): including Chat, Message and User models

---
+++ backend/models/userModel.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userModel = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    picture: {
+      type: String,
+      required: true,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userModel);
+
+export default User;
