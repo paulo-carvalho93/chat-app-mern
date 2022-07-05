1ec696008786691c9f6efaf3aec54b83f8637d90
Paulo Carvalho <pauloej.carvalho@gmail.com>
2022-07-05 09:40:01 -0300

    feat(chat): including chat page structure

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,14 +1,27 @@
+import { Box } from "@chakra-ui/react";
 import { useEffect } from "react";
 
+import ChatBox from "../components/ChatBox";
+import MyChats from "../components/MyChats";
+import SideDrawer from "../components/SideDrawer";
+import { useChatContext } from "../context/chatContext";
+
 export default function Chat() {
-  // const fetchChats = async () => {
-  //   const data = await axios.get("/api/chat");
-  //   console.log(data);
-  // };
+  const { user } = useChatContext();
 
-  // useEffect(() => {
-  //   fetchChats();
-  // }, []);
-
-  return <div>Chat</div>;
+  return (
+    <div style={{ width: "100%" }}>
+      {user && <SideDrawer />}
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        w="100%"
+        h="91.5vh"
+        p="0.625rem"
+      >
+        {user && <MyChats />}
+        {user && <ChatBox />}
+      </Box>
+    </div>
+  );
 }
