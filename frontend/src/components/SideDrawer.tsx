3d08642f30b01f6c09510c6c271719ff645dc9de
Paulo Carvalho <pauloej.carvalho@gmail.com>
2022-07-08 15:52:45 -0300

    feat(sidedrawer): creating SideDrawer component structure

--- frontend/src/components/SideDrawer.tsx
+++ frontend/src/components/SideDrawer.tsx
@@ -1,5 +1,77 @@
-import * as React from "react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+  Text,
+  Tooltip,
+} from "@chakra-ui/react";
+import { Bell, MagnifyingGlass, CaretDown } from "phosphor-react";
+import { useState } from "react";
+
+import { useChatContext } from "../context/chatContext";
 
 export default function SideDrawer() {
-  return <div></div>;
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loadingChat, setLoadingChat] = useState();
+
+  const { user } = useChatContext();
+
+  return (
+    <>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        bg="white"
+        w="100%"
+        p="0.313rem 0.625rem 0.313rem 0.625rem"
+        borderWidth="0.313rem"
+        borderRadius="2xl"
+      >
+        <Tooltip label="Search users to chat" hasArrow placement="bottom-end">
+          <Button variant="ghost">
+            <MagnifyingGlass size={24} color="#7C7C7C" />
+            <Text display={{ base: "none", md: "flex" }} px="4" color="#7C7C7C">
+              Search User
+            </Text>
+          </Button>
+        </Tooltip>
+
+        <Text fontSize="2xl" fontFamily="Roboto">
+          Chat App
+        </Text>
+
+        <div>
+          <Menu>
+            <MenuButton p={1} margin="1">
+              <Bell size={24} />
+            </MenuButton>
+            {/* <MenuList></MenuList> */}
+          </Menu>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<CaretDown size={12} />}>
+              <Avatar
+                size="sm"
+                cursor="pointer"
+                name={user.name}
+                src={user.picture}
+              />
+            </MenuButton>
+            <MenuList>
+              <MenuItem>My Profile</MenuItem>
+              <MenuDivider />
+              <MenuItem>Logout</MenuItem>
+            </MenuList>
+          </Menu>
+        </div>
+      </Box>
+    </>
+  );
 }
